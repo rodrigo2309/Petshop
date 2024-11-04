1efd48af3d798e5c2fc8c79496732aa6e6b61f89
RODRIGO MORI BEZERRA CUNHA <rodrigo.bcunha@claro.com.br>
2024-11-04 08:35:47 -0300

    Configurado rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,27 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { PetPagesComponent } from './pages/account/pet-pages/pet-pages.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 
 export const routes: Routes = [
   {
-    path: 'login',
-    component: LoginPageComponent,
+    path: '',
+    component: FramePageComponent,
+    children: [
+      { path: '', component: ProductsPageComponent },
+      { path: 'cart', component: CartPageComponent },
+    ],
   },
+  {
+    path: 'account',
+    component: FramePageComponent,
+    children: [{ path: 'pets', component: PetPagesComponent }],
+  },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'reset-password', component: ResetPasswordPageComponent },
 ];
--- src/app/pages/account/login-page/login-page.component.ts
+++ src/app/pages/account/login-page/login-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './login-page.component.html',
 })
 export class LoginPageComponent {}
